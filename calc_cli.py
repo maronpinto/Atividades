920c0f133f799f0b866f042c64bd3528b1aaa326
maronpinto <maronpinto@gmail.com>
2017-06-22 17:19:26 -0300

    Add files via upload

--- calc_cli.py
+++ calc_cli.py
@@ -1,24 +1,64 @@
 import socket
+import os
+
 serverName = 'localhost'
 serverPort = 2000
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-operacao = input("Digite a operacao: ")
-if operacao == 'soma':
-	print("Calcular a soma\n")
-	a = input("Digite o valor 1 ")
-	b = input("Digite o valor 2 ")
-	message = "soma " + a + " " +  b + "\n"
-elif operacao == "raiz":
-	print("Calcular raiz quadrade de\n")
-	a = input("Digite o valor ")
-elif operacao == 'quit':
-	message = ""
-    
-byte_msg = message.encode('utf-8')
 
-clientSocket.sendto(byte_msg, (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage)
-print(modifiedMessage.decode('utf-8'))
+os.system('clear')
+#print('                   CALCULADORA                        ')
+#print('------------------------------------------------------')
+#operacao = input(' Operacao - 1 Soma, 2 Raiz ou 0 Sair => ')
+operacao = ""
+
+out_oper = 0
+while operacao != '0':
+	
+	if out_oper == '1':
+		os.system('clear') 
+
+	print('                   CALCULADORA                        ')
+	print('------------------------------------------------------')
+	operacao = input(' Operacao - 1 Soma, 2 Raiz ou 0 Sair => ')
+	print('------------------------------------------------------\n')
+
+
+	if operacao == '1':
+		a = input(" Digite o parcela 1 => ")
+		print('')
+		b = input(" Digite o parcela 2 => ")
+		message = "soma " + a + " " +  b + "\n"
+		retorno = " A soma entre " + a + " e " + b + " e => "  
+
+
+	elif operacao == "2":
+		a = input(" Digite o Radicando ")
+		message = "raiz " + a + "\n"
+		retorno = " A raiz de " + a + " e => " 
+
+	elif (operacao) == '0':
+		message = "fim"
+		break
+
+	else:
+		print(' Operacao invalida')
+
+
+	if message != " ":
+		byte_msg = message.encode('utf-8')
+		clientSocket.sendto(byte_msg, (serverName, serverPort))
+
+	modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+	print('\n')
+	print(retorno + modifiedMessage.decode('utf-8') + "\n")
+	print('------------------------------------------------------')
+	
+	out_oper = input(" Fazer outro calculo ? 1 Sim ou 0 Nao => ")
+	if out_oper == '0':
+		os.system('clear')
+		operacao = '0'
+	else:
+		os.system('clear')
+	
 clientSocket.close()
--- calc_serv.py
+++ calc_serv.py
@@ -4,26 +4,45 @@ import os
 serverHost = 'localhost'
 serverPort = 2000
 
+os.system('clear')
+print('                   CALCULADORA                        ')
+print('------------------------------------------------------')
+print(' Esperando Operacao                                   ')
+
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind((serverHost, serverPort))
-os.system('clear')
-print("Servidor pronto para receber: ")
 
 
 soma = 0
 while True:
 
 	message, clientAddress = serverSocket.recvfrom(2048)
-	print('Conexão de: ', clientAddress)
+	os.system('clear')
+	#print('Conexão de: ', clientAddress)
     #Processa a stringe para lestras maiusculas
 
 	mens = message.decode('utf-8')
 
 	operacao = mens.split()
-	print(operacao)
-	print(operacao[1])
+
+	print('                   CALCULADORA                        ')
+	print('------------------------------------------------------')
+	print(' Esperando Operacao                                   ')
+
+
+	print(' Conexao de : ', clientAddress)
+
 	if operacao[0] == 'soma':
 		soma = int(operacao[1]) + int(operacao[2])
-	print(soma)
+		print(' Calcular a %s entre %s e %s = %s \n ' %(operacao[0], operacao[1], operacao[2], soma))
+		message = str(soma).encode('utf-8')
+	
+	elif operacao[0] == 'raiz':
+		raiz = (int(operacao[1])**0.5)
+		print(' Calcular a Raiz Quadradade de %s = ' %(operacao[1]), raiz)
+	
+	
+		message = str(raiz).encode()
+	
 
 	serverSocket.sendto(message, clientAddress)
